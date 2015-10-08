bbeba4232c14aaf73417c947ac8c27860b4199be
Bram de Boer <bramdeboer@outlook.com>
2015-10-08 13:13:16 +0200

    Alle classes geimplementeerd

--- ICT4Rails/ICT4Rails/Classes/TramManager.cs
+++ ICT4Rails/ICT4Rails/Classes/TramManager.cs
@@ -8,5 +8,24 @@ namespace ICT4Rails.Classes
 {
 	class TramManager
 	{
+		private List<Tram> trams = new List<Tram>();
+
+		public void ClearSector(Sector sector)
+		{
+
+		}
+		public void AddTramToSector(Tram tram, Sector sector, DateTime beginDate, DateTime endDate)
+		{
+
+		}
+
+		public void SetTramDirty(Tram tram, String comment)
+		{
+
+		}
+		public void SetTramMaintenens(Tram tram, String comment)
+		{
+
+		}
 	}
 }
