dcfd02a8bd5c517ab61f13be17adbbaa315ce99a
Luud van keulen <l.vankeulen@student.fontys.nl>
2015-10-08 15:13:08 +0200

    Testuser fix

--- ICT4Rails/ICT4Rails/Classes/Program.cs
+++ ICT4Rails/ICT4Rails/Classes/Program.cs
@@ -18,8 +18,7 @@ namespace ICT4Rails
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
-            /*if (!DatabaseManager.CheckConnection())
+            if (!DatabaseManager.CheckConnection())
             {
                 MessageBox.Show("De applicatie kan geen verbinding maken met de database. Neem contact op met een systeembeheerder.");
                 Application.Exit();
@@ -27,7 +26,7 @@ namespace ICT4Rails
             else
             {
                 Application.Run(new LoginForm());
-            }*/
+            }
         }
     }
 }
