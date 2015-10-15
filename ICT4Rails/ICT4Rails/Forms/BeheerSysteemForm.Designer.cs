34d10488230253b88083ab3d35a53c51bdb75485
Theun Schut <theun_96@hotmail.com>
2015-10-15 13:34:59 +0200

    GUI beheersysteem gemaakt

--- ICT4Rails/ICT4Rails/Forms/BeheerSysteemForm.Designer.cs
+++ ICT4Rails/ICT4Rails/Forms/BeheerSysteemForm.Designer.cs
@@ -28,41 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnRepairTrain = new System.Windows.Forms.Button();
-            this.btnCleanTrain = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
-            this.listUsers = new System.Windows.Forms.ListView();
-            this.Tramnummer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Rail = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Status = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.tbComment = new System.Windows.Forms.TextBox();
+            this.tlpGrid = new System.Windows.Forms.TableLayoutPanel();
             this.SuspendLayout();
-            // 
-            // btnRepairTrain
-            // 
-            this.btnRepairTrain.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnRepairTrain.BackColor = System.Drawing.Color.Silver;
-            this.btnRepairTrain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnRepairTrain.Font = new System.Drawing.Font("Arial", 15.75F);
-            this.btnRepairTrain.Location = new System.Drawing.Point(565, 529);
-            this.btnRepairTrain.Name = "btnRepairTrain";
-            this.btnRepairTrain.Size = new System.Drawing.Size(356, 108);
-            this.btnRepairTrain.TabIndex = 10;
-            this.btnRepairTrain.Text = "Repareren";
-            this.btnRepairTrain.UseVisualStyleBackColor = false;
-            // 
-            // btnCleanTrain
-            // 
-            this.btnCleanTrain.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnCleanTrain.BackColor = System.Drawing.Color.Silver;
-            this.btnCleanTrain.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnCleanTrain.Font = new System.Drawing.Font("Arial", 15.75F);
-            this.btnCleanTrain.Location = new System.Drawing.Point(195, 529);
-            this.btnCleanTrain.Name = "btnCleanTrain";
-            this.btnCleanTrain.Size = new System.Drawing.Size(356, 108);
-            this.btnCleanTrain.TabIndex = 8;
-            this.btnCleanTrain.Text = "Schoonmaken";
-            this.btnCleanTrain.UseVisualStyleBackColor = false;
             // 
             // btnBack
             // 
@@ -78,48 +46,73 @@
             this.btnBack.UseVisualStyleBackColor = false;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
-            // listUsers
+            // tlpGrid
             // 
-            this.listUsers.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.listUsers.BackColor = System.Drawing.Color.Silver;
-            this.listUsers.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.Tramnummer,
-            this.Rail,
-            this.Status});
-            this.listUsers.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listUsers.Location = new System.Drawing.Point(195, 77);
-            this.listUsers.Name = "listUsers";
-            this.listUsers.Size = new System.Drawing.Size(726, 395);
-            this.listUsers.TabIndex = 12;
-            this.listUsers.UseCompatibleStateImageBehavior = false;
-            this.listUsers.View = System.Windows.Forms.View.Details;
-            // 
-            // Tramnummer
-            // 
-            this.Tramnummer.Text = "Tramnummer";
-            this.Tramnummer.Width = 363;
-            // 
-            // Rail
-            // 
-            this.Rail.Text = "Rail";
-            this.Rail.Width = 100;
-            // 
-            // Status
-            // 
-            this.Status.Text = "Status";
-            this.Status.Width = 359;
-            // 
-            // tbComment
-            // 
-            this.tbComment.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.tbComment.Location = new System.Drawing.Point(195, 478);
-            this.tbComment.Multiline = true;
-            this.tbComment.Name = "tbComment";
-            this.tbComment.Size = new System.Drawing.Size(726, 45);
-            this.tbComment.TabIndex = 13;
-            this.tbComment.Text = "Enter comment here...";
-            this.tbComment.Enter += new System.EventHandler(this.tbComment_Enter);
-            this.tbComment.Leave += new System.EventHandler(this.tbComment_Leave);
+            this.tlpGrid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpGrid.AutoSize = true;
+            this.tlpGrid.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tlpGrid.ColumnCount = 19;
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.263158F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpGrid.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpGrid.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tlpGrid.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tlpGrid.Location = new System.Drawing.Point(12, 12);
+            this.tlpGrid.Name = "tlpGrid";
+            this.tlpGrid.RowCount = 29;
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.448276F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpGrid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlpGrid.Size = new System.Drawing.Size(1093, 639);
+            this.tlpGrid.TabIndex = 12;
             // 
             // BeheerSysteemForm
             // 
@@ -128,11 +121,8 @@
             this.BackgroundImage = global::ICT4Rails.Properties.Resources.darkrailbackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1117, 708);
-            this.Controls.Add(this.tbComment);
-            this.Controls.Add(this.listUsers);
+            this.Controls.Add(this.tlpGrid);
             this.Controls.Add(this.btnBack);
-            this.Controls.Add(this.btnRepairTrain);
-            this.Controls.Add(this.btnCleanTrain);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MaximizeBox = false;
@@ -146,14 +136,7 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnRepairTrain;
-        private System.Windows.Forms.Button btnCleanTrain;
         private System.Windows.Forms.Button btnBack;
-        private System.Windows.Forms.ListView listUsers;
-        private System.Windows.Forms.ColumnHeader Tramnummer;
-        private System.Windows.Forms.ColumnHeader Status;
-        private System.Windows.Forms.ColumnHeader Rail;
-        private System.Windows.Forms.TextBox tbComment;
+        private System.Windows.Forms.TableLayoutPanel tlpGrid;
     }
 }
--- ICT4Rails/ICT4Rails/Forms/BeheerSysteemForm.cs
+++ ICT4Rails/ICT4Rails/Forms/BeheerSysteemForm.cs
@@ -12,27 +12,149 @@ namespace ICT4Rails
 {
     public partial class BeheerSysteemForm : Form
     {
+        List<Label> labels = new List<Label>();
+        List<string> locations = new List<string>();
+
         public BeheerSysteemForm()
         {
             InitializeComponent();
+
+            StandardRailLocation();
+
+            /*
+            for (int row = 0; row < tlpGrid.RowCount; row++)
+            {
+                for (int column = 0; column < tlpGrid.ColumnCount; column++)
+                {
+                    if (column == 0 && row == 2)
+                    {
+                        
+                    }
+                }
+            }
+            */
+
+            UpdateGrid();
         }
 
         private void btnBack_Click(object sender, EventArgs e)
         {
-            Close();
+            this.Close();
         }
 
-        private void tbComment_Enter(object sender, EventArgs e)
+        private Label AddLabel(string ColumnRow, string text)
         {
-            tbComment.Text = "";
+            Label l = new Label();
+
+            l.Dock = DockStyle.Fill;
+            l.Margin = new Padding(1);
+            l.Text = text;
+            l.ForeColor = Color.Black;
+            l.TextAlign = ContentAlignment.MiddleCenter;
+
+            l.Tag = ColumnRow;
+
+            return l;
         }
 
-        private void tbComment_Leave(object sender, EventArgs e)
+        private void UpdateGrid()
         {
-            if(tbComment.Text == "")
+            foreach (Label l in labels)
             {
-                tbComment.Text = "Enter comment here...";
+                string columnString = l.Tag.ToString();
+                string rowString = l.Tag.ToString();
+
+                int spaceIndex = columnString.IndexOf(" ");
+
+                columnString = columnString.Substring(0, spaceIndex);
+                rowString = rowString.Substring(spaceIndex);
+
+                int column = Convert.ToInt32(columnString);
+                int row = Convert.ToInt32(rowString);
+
+                tlpGrid.Controls.Add(l, column, row);
             }
+        }
+
+        private void StandardRailLocation()
+        {
+            int column = 0;
+            int row = 0;
+
+            for(int i = 38; i >= 30; i--)
+            {
+                AddRail(column + " " + row, i.ToString());
+                column++;
+            }
+
+            column++;
+
+            for(int i = 40; i <= 44; i++)
+            {
+                AddRail(column + " " + row, i.ToString());
+                column++;
+            }
+
+            column++;
+
+            AddRail(column + " " + row, "45");
+            column += 2;
+
+            AddRail(column + " " + row, "58");
+
+            column = 0;
+            row = 13;
+            
+            for(int i = 57; i >= 51; i--)
+            {
+                AddRail(column + " " + row, i.ToString());
+                column++;
+            }
+
+            for(int i = 64; i >= 61; i--)
+            {
+                AddRail(column + " " + row, i.ToString());
+                column++;
+            }
+
+            column++;
+
+            for(int i = 74; i <= 77; i++)
+            {
+                AddRail(column + " " + row, i.ToString());
+                column++;
+            }
+
+            column++;
+
+            for(int i = 12; i <= 21; i++)
+            {
+                AddRail(column + " " + row, i.ToString());
+                row++;
+            }
+        }
+
+        private void AddRail(string location, string text)
+        {
+            Label l = AddLabel(location, text);
+
+            l.BackColor = Color.LightGray;
+
+            labels.Add(l);
+        }
+
+        private void AddSector(string location, string text)
+        {
+            Label l = AddLabel(location, text);
+
+            l.Click += new EventHandler(Label_Click);
+
+            labels.Add(l);
+        }
+
+        private void Label_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("het werkt lul");
         }
     }
 }
