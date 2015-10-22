62167e93ee3ea61d2b92e43256823a90d3a7a812
Niekvdh <niek.heijden@hotmail.nl>
2015-10-22 12:05:29 +0200

    RFID

--- ICT4Rails/ICT4Rails/Forms/InUitRijSyteemForm.Designer.cs
+++ ICT4Rails/ICT4Rails/Forms/InUitRijSyteemForm.Designer.cs
@@ -56,10 +56,9 @@
             this.btnBack.BackColor = System.Drawing.Color.Silver;
             this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnBack.Font = new System.Drawing.Font("Arial", 15.75F);
-            this.btnBack.Location = new System.Drawing.Point(1188, 809);
-            this.btnBack.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBack.Location = new System.Drawing.Point(891, 657);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(285, 48);
+            this.btnBack.Size = new System.Drawing.Size(214, 39);
             this.btnBack.TabIndex = 11;
             this.btnBack.Text = "Terug";
             this.btnBack.UseVisualStyleBackColor = false;
@@ -71,19 +70,21 @@
             this.txtTramNumber.BackColor = System.Drawing.SystemColors.Control;
             this.txtTramNumber.Enabled = false;
             this.txtTramNumber.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTramNumber.Location = new System.Drawing.Point(534, 94);
+            this.txtTramNumber.Location = new System.Drawing.Point(400, 76);
+            this.txtTramNumber.Margin = new System.Windows.Forms.Padding(2);
             this.txtTramNumber.MaxLength = 5;
             this.txtTramNumber.Name = "txtTramNumber";
-            this.txtTramNumber.Size = new System.Drawing.Size(400, 145);
+            this.txtTramNumber.Size = new System.Drawing.Size(301, 118);
             this.txtTramNumber.TabIndex = 12;
             // 
             // btnNumber1
             // 
             this.btnNumber1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber1.Location = new System.Drawing.Point(534, 245);
+            this.btnNumber1.Location = new System.Drawing.Point(400, 199);
+            this.btnNumber1.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber1.Name = "btnNumber1";
-            this.btnNumber1.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber1.Size = new System.Drawing.Size(98, 106);
             this.btnNumber1.TabIndex = 13;
             this.btnNumber1.Text = "1";
             this.btnNumber1.UseVisualStyleBackColor = true;
@@ -93,9 +94,10 @@
             // 
             this.btnNumber2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber2.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber2.Location = new System.Drawing.Point(670, 245);
+            this.btnNumber2.Location = new System.Drawing.Point(502, 199);
+            this.btnNumber2.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber2.Name = "btnNumber2";
-            this.btnNumber2.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber2.Size = new System.Drawing.Size(98, 106);
             this.btnNumber2.TabIndex = 14;
             this.btnNumber2.Text = "2";
             this.btnNumber2.UseVisualStyleBackColor = true;
@@ -105,9 +107,10 @@
             // 
             this.btnNumber3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber3.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber3.Location = new System.Drawing.Point(804, 245);
+            this.btnNumber3.Location = new System.Drawing.Point(603, 199);
+            this.btnNumber3.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber3.Name = "btnNumber3";
-            this.btnNumber3.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber3.Size = new System.Drawing.Size(98, 106);
             this.btnNumber3.TabIndex = 15;
             this.btnNumber3.Text = "3";
             this.btnNumber3.UseVisualStyleBackColor = true;
@@ -117,9 +120,10 @@
             // 
             this.btnNumber6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber6.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber6.Location = new System.Drawing.Point(804, 381);
+            this.btnNumber6.Location = new System.Drawing.Point(603, 310);
+            this.btnNumber6.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber6.Name = "btnNumber6";
-            this.btnNumber6.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber6.Size = new System.Drawing.Size(98, 106);
             this.btnNumber6.TabIndex = 18;
             this.btnNumber6.Text = "6";
             this.btnNumber6.UseVisualStyleBackColor = true;
@@ -129,9 +133,10 @@
             // 
             this.btnNumber5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber5.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber5.Location = new System.Drawing.Point(670, 381);
+            this.btnNumber5.Location = new System.Drawing.Point(502, 310);
+            this.btnNumber5.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber5.Name = "btnNumber5";
-            this.btnNumber5.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber5.Size = new System.Drawing.Size(98, 106);
             this.btnNumber5.TabIndex = 17;
             this.btnNumber5.Text = "5";
             this.btnNumber5.UseVisualStyleBackColor = true;
@@ -141,9 +146,10 @@
             // 
             this.btnNumber4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber4.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber4.Location = new System.Drawing.Point(534, 381);
+            this.btnNumber4.Location = new System.Drawing.Point(400, 310);
+            this.btnNumber4.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber4.Name = "btnNumber4";
-            this.btnNumber4.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber4.Size = new System.Drawing.Size(98, 106);
             this.btnNumber4.TabIndex = 16;
             this.btnNumber4.Text = "4";
             this.btnNumber4.UseVisualStyleBackColor = true;
@@ -153,9 +159,10 @@
             // 
             this.btnNumber9.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber9.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber9.Location = new System.Drawing.Point(804, 517);
+            this.btnNumber9.Location = new System.Drawing.Point(603, 420);
+            this.btnNumber9.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber9.Name = "btnNumber9";
-            this.btnNumber9.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber9.Size = new System.Drawing.Size(98, 106);
             this.btnNumber9.TabIndex = 21;
             this.btnNumber9.Text = "9";
             this.btnNumber9.UseVisualStyleBackColor = true;
@@ -165,9 +172,10 @@
             // 
             this.btnNumber8.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber8.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber8.Location = new System.Drawing.Point(670, 517);
+            this.btnNumber8.Location = new System.Drawing.Point(502, 420);
+            this.btnNumber8.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber8.Name = "btnNumber8";
-            this.btnNumber8.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber8.Size = new System.Drawing.Size(98, 106);
             this.btnNumber8.TabIndex = 20;
             this.btnNumber8.Text = "8";
             this.btnNumber8.UseVisualStyleBackColor = true;
@@ -177,9 +185,10 @@
             // 
             this.btnNumber7.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber7.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber7.Location = new System.Drawing.Point(534, 517);
+            this.btnNumber7.Location = new System.Drawing.Point(400, 420);
+            this.btnNumber7.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber7.Name = "btnNumber7";
-            this.btnNumber7.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber7.Size = new System.Drawing.Size(98, 106);
             this.btnNumber7.TabIndex = 19;
             this.btnNumber7.Text = "7";
             this.btnNumber7.UseVisualStyleBackColor = true;
@@ -189,9 +198,10 @@
             // 
             this.btnNumber0.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnNumber0.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNumber0.Location = new System.Drawing.Point(670, 653);
+            this.btnNumber0.Location = new System.Drawing.Point(502, 531);
+            this.btnNumber0.Margin = new System.Windows.Forms.Padding(2);
             this.btnNumber0.Name = "btnNumber0";
-            this.btnNumber0.Size = new System.Drawing.Size(130, 130);
+            this.btnNumber0.Size = new System.Drawing.Size(98, 106);
             this.btnNumber0.TabIndex = 22;
             this.btnNumber0.Text = "0";
             this.btnNumber0.UseVisualStyleBackColor = true;
@@ -201,9 +211,10 @@
             // 
             this.btnBackspace.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnBackspace.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnBackspace.Location = new System.Drawing.Point(534, 653);
+            this.btnBackspace.Location = new System.Drawing.Point(400, 531);
+            this.btnBackspace.Margin = new System.Windows.Forms.Padding(2);
             this.btnBackspace.Name = "btnBackspace";
-            this.btnBackspace.Size = new System.Drawing.Size(130, 130);
+            this.btnBackspace.Size = new System.Drawing.Size(98, 106);
             this.btnBackspace.TabIndex = 23;
             this.btnBackspace.Text = "«";
             this.btnBackspace.UseVisualStyleBackColor = true;
@@ -213,9 +224,10 @@
             // 
             this.btnConfirm.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnConfirm.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnConfirm.Location = new System.Drawing.Point(804, 653);
+            this.btnConfirm.Location = new System.Drawing.Point(603, 531);
+            this.btnConfirm.Margin = new System.Windows.Forms.Padding(2);
             this.btnConfirm.Name = "btnConfirm";
-            this.btnConfirm.Size = new System.Drawing.Size(130, 130);
+            this.btnConfirm.Size = new System.Drawing.Size(98, 106);
             this.btnConfirm.TabIndex = 24;
             this.btnConfirm.Text = "Enter";
             this.btnConfirm.UseVisualStyleBackColor = true;
@@ -226,9 +238,10 @@
             this.btnRecover.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.btnRecover.Enabled = false;
             this.btnRecover.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRecover.Location = new System.Drawing.Point(534, 789);
+            this.btnRecover.Location = new System.Drawing.Point(400, 641);
+            this.btnRecover.Margin = new System.Windows.Forms.Padding(2);
             this.btnRecover.Name = "btnRecover";
-            this.btnRecover.Size = new System.Drawing.Size(400, 62);
+            this.btnRecover.Size = new System.Drawing.Size(300, 50);
             this.btnRecover.TabIndex = 25;
             this.btnRecover.Text = "Herstel";
             this.btnRecover.UseVisualStyleBackColor = true;
@@ -240,9 +253,10 @@
             this.txtDesiredRail.BackColor = System.Drawing.SystemColors.Control;
             this.txtDesiredRail.Enabled = false;
             this.txtDesiredRail.Font = new System.Drawing.Font("Arial", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDesiredRail.Location = new System.Drawing.Point(60, 94);
+            this.txtDesiredRail.Location = new System.Drawing.Point(45, 76);
+            this.txtDesiredRail.Margin = new System.Windows.Forms.Padding(2);
             this.txtDesiredRail.Name = "txtDesiredRail";
-            this.txtDesiredRail.Size = new System.Drawing.Size(376, 145);
+            this.txtDesiredRail.Size = new System.Drawing.Size(283, 118);
             this.txtDesiredRail.TabIndex = 26;
             // 
             // lblDesiredRail
@@ -252,9 +266,10 @@
             this.lblDesiredRail.BackColor = System.Drawing.Color.Transparent;
             this.lblDesiredRail.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDesiredRail.ForeColor = System.Drawing.Color.White;
-            this.lblDesiredRail.Location = new System.Drawing.Point(50, 36);
+            this.lblDesiredRail.Location = new System.Drawing.Point(38, 29);
+            this.lblDesiredRail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblDesiredRail.Name = "lblDesiredRail";
-            this.lblDesiredRail.Size = new System.Drawing.Size(386, 55);
+            this.lblDesiredRail.Size = new System.Drawing.Size(304, 43);
             this.lblDesiredRail.TabIndex = 27;
             this.lblDesiredRail.Text = "Gewenste spoor:";
             // 
@@ -265,9 +280,10 @@
             this.lblTramNumber.BackColor = System.Drawing.Color.Transparent;
             this.lblTramNumber.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblTramNumber.ForeColor = System.Drawing.Color.White;
-            this.lblTramNumber.Location = new System.Drawing.Point(524, 36);
+            this.lblTramNumber.Location = new System.Drawing.Point(393, 29);
+            this.lblTramNumber.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTramNumber.Name = "lblTramNumber";
-            this.lblTramNumber.Size = new System.Drawing.Size(323, 55);
+            this.lblTramNumber.Size = new System.Drawing.Size(260, 43);
             this.lblTramNumber.TabIndex = 28;
             this.lblTramNumber.Text = "Tramnummer:";
             // 
@@ -277,9 +293,10 @@
             this.checkRepair.Appearance = System.Windows.Forms.Appearance.Button;
             this.checkRepair.AutoSize = true;
             this.checkRepair.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkRepair.Location = new System.Drawing.Point(60, 272);
+            this.checkRepair.Location = new System.Drawing.Point(45, 221);
+            this.checkRepair.Margin = new System.Windows.Forms.Padding(2);
             this.checkRepair.Name = "checkRepair";
-            this.checkRepair.Size = new System.Drawing.Size(374, 65);
+            this.checkRepair.Size = new System.Drawing.Size(296, 53);
             this.checkRepair.TabIndex = 0;
             this.checkRepair.Text = "Reparatie nodig";
             this.checkRepair.UseVisualStyleBackColor = true;
@@ -291,20 +308,21 @@
             this.checkCleaning.AutoSize = true;
             this.checkCleaning.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.checkCleaning.Font = new System.Drawing.Font("Arial", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkCleaning.Location = new System.Drawing.Point(25, 343);
+            this.checkCleaning.Location = new System.Drawing.Point(19, 279);
+            this.checkCleaning.Margin = new System.Windows.Forms.Padding(2);
             this.checkCleaning.Name = "checkCleaning";
-            this.checkCleaning.Size = new System.Drawing.Size(447, 65);
+            this.checkCleaning.Size = new System.Drawing.Size(355, 53);
             this.checkCleaning.TabIndex = 1;
             this.checkCleaning.Text = "Schoonmaak nodig";
             this.checkCleaning.UseVisualStyleBackColor = true;
             // 
             // InUitRijSyteemForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::ICT4Rails.Properties.Resources.darkrailbackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1489, 871);
+            this.ClientSize = new System.Drawing.Size(1034, 634);
             this.Controls.Add(this.checkCleaning);
             this.Controls.Add(this.checkRepair);
             this.Controls.Add(this.lblTramNumber);
@@ -327,12 +345,12 @@
             this.Controls.Add(this.btnBack);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "InUitRijSyteemForm";
             this.ShowIcon = false;
             this.Text = "OverviewForm";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Shown += new System.EventHandler(this.InUitRijSyteemForm_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- ICT4Rails/ICT4Rails/Forms/InUitRijSyteemForm.cs
+++ ICT4Rails/ICT4Rails/Forms/InUitRijSyteemForm.cs
@@ -7,17 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Phidgets;
+using Phidgets.Events;
 
 namespace ICT4Rails
 {
     public partial class InUitRijSyteemForm : Form
     {
+        private RFID rfid;
+
         private Button[] buttons;
         public InUitRijSyteemForm()
         {
             InitializeComponent();
             buttons = new Button[] { btnNumber0, btnNumber1, btnNumber2, btnNumber3, btnNumber4, btnNumber5, btnNumber6, btnNumber7, btnNumber8, btnNumber9, btnNumber0, btnBackspace, btnConfirm };
         }
+
+
+        void rfid_Tag(object sender, TagEventArgs e)
+        {
+            
+            if (e.Tag == "2800a7bcc2")
+            {
+                txtTramNumber.Text = "1";
+            }
+            else if (e.Tag == "28002b4194")
+            {
+                txtTramNumber.Text = "2";
+            }
+
+            else if (e.Tag == "1100ad7362")
+            {
+                txtTramNumber.Text = "3";
+            }
+            
+        }
+
 
         private void btnBack_Click(object sender, EventArgs e)
         {
@@ -65,5 +90,15 @@ namespace ICT4Rails
             checkRepair.Enabled = true;
             checkCleaning.Enabled = true;
         }
+
+        private void InUitRijSyteemForm_Shown(object sender, EventArgs e)
+        {
+            rfid = new RFID();
+
+            rfid.Tag += new TagEventHandler(rfid_Tag);
+            rfid.open();
+        }
+
+       
     }
 }
