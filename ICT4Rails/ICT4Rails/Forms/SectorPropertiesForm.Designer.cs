f25c7615b1028ff7864b5f45a4ac86d8b75b62b9
Theun Schut <theun_96@hotmail.com>
2015-10-22 10:48:22 +0200

    beheersysteem veranderingen

---
+++ ICT4Rails/ICT4Rails/Forms/SectorPropertiesForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ICT4Rails
+{
+    partial class SectorPropertiesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBack = new System.Windows.Forms.Button();
+            this.cbBlocked = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(577, 360);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "Terug";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // cbBlocked
+            // 
+            this.cbBlocked.AutoSize = true;
+            this.cbBlocked.Location = new System.Drawing.Point(12, 12);
+            this.cbBlocked.Name = "cbBlocked";
+            this.cbBlocked.Size = new System.Drawing.Size(87, 17);
+            this.cbBlocked.TabIndex = 1;
+            this.cbBlocked.Text = "Geblokkeerd";
+            this.cbBlocked.UseVisualStyleBackColor = true;
+            // 
+            // SectorPropertiesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 395);
+            this.Controls.Add(this.cbBlocked);
+            this.Controls.Add(this.btnBack);
+            this.Name = "SectorPropertiesForm";
+            this.Text = "SectorPropertiesForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.CheckBox cbBlocked;
+    }
+}
