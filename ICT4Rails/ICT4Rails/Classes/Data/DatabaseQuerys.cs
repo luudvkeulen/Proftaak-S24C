6895b168df00830de86db3cec90fc97171c7e20e
Luud van keulen <l.vankeulen@student.fontys.nl>
2015-11-02 14:39:45 +0100

    Added finishing for techniek and schoonmaak

--- ICT4Rails/ICT4Rails/Classes/Data/DatabaseQuerys.cs
+++ ICT4Rails/ICT4Rails/Classes/Data/DatabaseQuerys.cs
@@ -11,6 +11,7 @@ namespace ICT4Rails
         {
             query["GetAllLogins"] = "SELECT * FROM EMPLOYEELOGINS";
             query["GetLogin"] = "SELECT * FROM EMPLOYEELOGINS WHERE USERNAME=:username";
+            query["GetEmployee"] = "SELECT USERID FROM EMPLOYEE WHERE name = :employee";
             query["CreateEmployee"] = "INSERT INTO EMPLOYEE(NAME, SURNAME, EMPLOYEETYPE) values (:name, :surname, :employeetype)";
             query["CreateLogin"] = "INSERT INTO LOGIN(USERNAME, PASSWORD, GUID, USERTYPE, USERID) values (:username, :password, :guid, :usertype, (SELECT MAX(USERID) FROM EMPLOYEE))";
 			query["GetTech"] = "SELECT m.TRAMID,m.DATEADDED,m.DATEFINISHED,m.FINISHEDBY, m.OPMERKING FROM MAINTENANCE m WHERE m.TYPE = 1";
@@ -20,6 +21,9 @@ namespace ICT4Rails
             query["GetAllSectors"] = "SELECT * FROM SECTOR";
             query["GetReservedSector"] = "SELECT RAILID, SECTORID FROM SECTOR WHERE TRAMID = :tramid AND ISRESERVED = 1";
 			query["UpdateEndDate"] = "UPDATE MAINTENANCE SET DATEFINISHED = TO_DATE(:datefinished,'dd/mm/yyyy hh24:mi:ss') WHERE TRAMID = :tramid";
-		}
+            query["GetAllCleaners"] = "SELECT NAME FROM EMPLOYEE WHERE EMPLOYEETYPE = 'schoonmaker'";
+            query["GetAllEngineers"] = "SELECT NAME FROM EMPLOYEE WHERE EMPLOYEETYPE = 'technicus'";
+            query["maintenancefinished"] = "UPDATE MAINTENANCE SET DATEFINISHED = TO_DATE(:datefinished,'dd/mm/yyyy hh24:mi:ss'), FINISHEDBY = :employeeid WHERE TRAMID = :tramid AND TYPE = :type";
+        }
     }
 }
